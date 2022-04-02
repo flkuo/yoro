d93f225bfbe8641f1f6d41cf4e0ed0f863fd3b32
flkuo <fkuo2@wisc.edu>
2022-04-02 22:48:56 +0800

    webpage updated

--- app_1/server.R
+++
@@ -1,9 +0,0 @@
-##server.R
-
-library(shiny)
-function(input, output) {
-  output$hist <- renderPlot({
-    title <- "100 random normal values"
-    hist(rnorm(input$num), main=title)
-  })
-}
--- app_1/ui.R
+++
@@ -1,9 +0,0 @@
-##ui.R
-
-library(shiny)
-fluidPage(
-  sliderInput(inputId = "num",
-              label= "Choose a number",
-              value= 25, min=1, max=100), 
-  plotOutput("hist")
-)
